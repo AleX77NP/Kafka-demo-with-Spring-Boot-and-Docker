2d1a47d9d75b5ee0711e02fd214708b7f521d18a
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2022-02-11 12:08:48 +0100

    producer

---
+++ kafka-producer-demo/src/main/java/com/ds/kafkaproducerdemo/KafkaProducerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ds.kafkaproducerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerDemoApplication.class, args);
+	}
+
+}
